7dbc382f5c5734938cd00568089ab577404373b5
so-su <komiyama.masaki.53w@st.kyoto-u.ac.jp>
2023-02-15 20:06:49 +0900

    ProgressBarのコードを整えた

--- src/ProgressBar.cpp
+++ src/ProgressBar.cpp
@@ -1,17 +1,21 @@
 #include "ProgressBar.hpp"
 
-ProgressBar::ProgressBar(Rect outer_bar, Color color)
+ProgressBar::ProgressBar(const Rect& outer_bar, Color color)
     : outer_bar(outer_bar.rounded(10)),
       inner_bar_full(outer_bar.stretched(-3)),
       inner_bar_mask(inner_bar_full.rounded(10).asPolygon()),
-      color(color),
-      ratio(1.0) {}
+      color(color) {}
 
+// バーの更新
 void ProgressBar::update(double ratio_) {
-    this->ratio = ratio_;
+    // 0.0〜1.0に収めて更新
+    this->ratio = std::clamp(ratio_, 0.0, 1.0);
 
-    Rect inner_bar_rect =
-        inner_bar_full.stretched(0, -inner_bar_full.w * (1.0 - ratio), 0, 0);
+    // 型抜きされる前のバー
+    const Rect inner_bar_rect{
+        inner_bar_full.stretched(0, -inner_bar_full.w * (1.0 - ratio), 0, 0)};
+    
+    // マスクで型抜きすることで、実際に描画する内部のバーを得る
     if (auto polygons = Geometry2D::And(inner_bar_rect, inner_bar_mask);
         polygons.empty()) {
         inner_bar_polygon = none;
@@ -20,9 +24,10 @@ void ProgressBar::update(double ratio_) {
     }
 }
 
+// バーの描画
 void ProgressBar::draw() const {
     outer_bar.draw(ColorF{0.5});
-    outer_bar.stretched(-3).draw(ColorF{0.2});
+    outer_bar.stretched(-3).draw(ColorF{0.2}); // 内部の背景
     if (inner_bar_polygon.has_value()) {
         inner_bar_polygon->draw(color);
     }
--- src/ProgressBar.hpp
+++ src/ProgressBar.hpp
@@ -2,23 +2,31 @@
 #include "Common.hpp"
 
 class ProgressBar {
-   private:
-    const RoundRect outer_bar;
+public:
+     ProgressBar(const Rect& outer_bar, Color color);
 
-    const Rect inner_bar_full;
-
-    Optional<Polygon> inner_bar_polygon;
-
-    const Polygon inner_bar_mask;
-
-    const Color color;
-
-    double ratio;
-
-   public:
-    ProgressBar(Rect outer_bar, Color color);
-
+    // バーの更新
     void update(double ratio);
 
+    // バーの描画
     void draw() const;
+    
+private:
+    // 外枠を含めた全体のRoundRect
+    const RoundRect outer_bar;
+
+    // 内部のRect
+    const Rect inner_bar_full;
+
+    // 実際に描画する内部のバー
+    Optional<Polygon> inner_bar_polygon;
+
+    // 実際に描画する内部のバーを得るために、共通部分を取るためのマスク
+    const Polygon inner_bar_mask;
+
+    // 内部のバーの色
+    const Color color;
+
+    // 内部のバーの割合（0.0〜1.0）
+    double ratio{1.0};
 };
