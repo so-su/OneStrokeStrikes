a77ade52c4ad95ee437f6a53bfb490f29256ba74
so-su <komiyama.masaki.53w@st.kyoto-u.ac.jp>
2023-03-19 07:03:12 +0900

    RingEffectのコードを整えた

--- src/RingEffect.cpp
+++ src/RingEffect.cpp
@@ -3,10 +3,11 @@
 RingEffect::RingEffect(Vec2 pos, int32 size)
     : pos{pos}, color{RandomColorF()}, size(size) {}
 
-bool RingEffect::update(double t) {
-    const double easing = EaseOutExpo(t);
+bool RingEffect::update(double time) {
+    // イージング関数の値
+    const double easing = EaseOutExpo(time);
 
     Circle{pos, easing * size}.drawFrame(size * (1.0 - easing), color);
 
-    return t < 1.0;
+    return time < 1.0;
 }
--- src/RingEffect.hpp
+++ src/RingEffect.hpp
@@ -5,12 +5,15 @@ struct RingEffect : IEffect {
    public:
     explicit RingEffect(Vec2 pos, int32 size);
 
-    bool update(double t) override;
+    bool update(double time) override;
 
    private:
+    // 中心の座標
     Vec2 pos;
 
+    // 色
     ColorF color;
 
+    // 大きさ
     int32 size;
 };
