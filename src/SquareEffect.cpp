980b07886ce140474243f5fb72caf65d963551bf
so-su <komiyama.masaki.53w@st.kyoto-u.ac.jp>
2023-03-19 06:59:37 +0900

    SquareEffectのコードを整えた

--- src/SquareEffect.cpp
+++ src/SquareEffect.cpp
@@ -1,40 +1,33 @@
 #include "SquareEffect.hpp"
 
-SquareEffect::SquareEffect(Vec2 pos, double base_hue)
-    : pos{ pos }
-{
-    for (auto _ :step(8))
-    {
-        Square square{
-            RandomVec2(Circle{30}),
-            Random(-0.3, 0.1),
-            Random(5.0, 40.0),
-            HSV{ base_hue + Random(-30.0, 30.0) }
-        };
-        squares << square;
+SquareEffect::SquareEffect(Vec2 pos, double base_hue) : pos{pos} {
+    for (auto square_idx : step(num_square)) {
+        squares[square_idx] =
+            Square{RandomVec2(Circle{30}), Random(-0.3, 0.1), Random(5.0, 40.0),
+                   HSV{base_hue + Random(-30.0, 30.0)}};
     }
 }
 
-bool SquareEffect::update(double t)
-{
-    for (const auto& square : squares)
-    {
-        const double t2 = (square.start_time + t);
+bool SquareEffect::update(double time) {
+    for (const auto& square : squares) {
+        // 描画が始まってからの経過時間
+        const double virtual_time{square.start_time + time};
 
-        if (not InRange(t2, 0.0, 1.0))
-        {
+        if (not InRange(virtual_time, 0.0, 1.0)) {
             continue;
         }
 
-        const double easing = EaseOutExpo(t2);
-        
-        const Vec2 square_pos=pos + square.offset +square.offset * 2 * t;
-        
-        const int32 square_size = static_cast<int32>(easing * square.scale);
-        
-        Rect{Arg::center=square_pos.asPoint(),square_size,square_size }.rotatedAt(square_pos,square_pos.x*6_deg).draw(ColorF{ square.color, 0.15 })
-                .drawFrame( 0.5 * (1.0 - easing) * square.scale, square.color);
+        // イージング関数の値
+        const double easing{EaseOutExpo(virtual_time)};
+
+        const Vec2 square_pos{pos + square.offset + square.offset * 2 * time};
+        const int32 square_size{static_cast<int32>(easing * square.scale)};
+
+        Rect{Arg::center = square_pos.asPoint(), square_size, square_size}
+            .rotatedAt(square_pos, square_pos.x * 6_deg)
+            .draw(ColorF{square.color, 0.15})
+            .drawFrame(0.5 * (1.0 - easing) * square.scale, square.color);
     }
 
-    return t < 1.3;
+    return time < 1.3;
 }
--- src/SquareEffect.hpp
+++ src/SquareEffect.hpp
@@ -1,23 +1,26 @@
 #pragma once
 #include"Common.hpp"
 
-struct SquareEffect : IEffect
-{
+struct SquareEffect : IEffect {
 public:
     SquareEffect(Vec2 pos, double base_hue);
 
-    bool update(double t) override;
+    bool update(double time) override;
     
 private:
-    struct Square
-    {
-        Vec2 offset;
-        double start_time;
-        double scale;
-        ColorF color;
+    struct Square {
+        Vec2 offset; // 中心からのオフセット
+        double start_time; // 描画が始まる時間
+        double scale; // 正方形の一辺の長さ
+        ColorF color; // 色
     };
 
+    // エフェクトの中心座標
     Vec2 pos;
+    
+    // 描画する正方形の個数
+    static constexpr int32 num_square{8};
 
-    Array<Square> squares;
+    // 描画する正方形の配列
+    std::array<Square, num_square> squares;
 };
