b78503987576f89feff3343cc7e1cdd59aefbd93
so-su <komiyama.masaki.53w@st.kyoto-u.ac.jp>
2023-03-19 06:11:57 +0900

    assemble.shを見やすくした

--- assemble.sh
+++ assemble.sh
@@ -2,4 +2,6 @@
 # 現状のSiv3D for Webは複数ファイルのコンパイルをサポートしてないため
 
 cd ./src/
-cat Common.hpp Parameter.hpp Button.hpp SimpleButton.hpp ProgressBar.hpp SquareEffect.hpp RingEffect.hpp AttackShape.hpp AttackRoulette.hpp Polyomino.hpp Enemy.hpp AlphaEnemy.hpp Player.hpp Title.hpp Game.hpp Result.hpp Ranking.hpp Button.cpp SimpleButton.cpp ProgressBar.cpp SquareEffect.cpp RingEffect.cpp AttackShape.cpp AttackRoulette.cpp Polyomino.cpp Enemy.cpp AlphaEnemy.cpp Player.cpp Title.cpp Game.cpp Result.cpp Ranking.cpp Main.cpp | sed 's/^#.*//' | sed '1s/^/#include <Siv3D.hpp>/'
+cat Common.hpp Parameter.hpp Button.hpp SimpleButton.hpp ProgressBar.hpp SquareEffect.hpp RingEffect.hpp AttackShape.hpp AttackRoulette.hpp Polyomino.hpp Enemy.hpp AlphaEnemy.hpp Player.hpp Title.hpp Game.hpp Result.hpp Ranking.hpp \
+Button.cpp SimpleButton.cpp ProgressBar.cpp SquareEffect.cpp RingEffect.cpp AttackShape.cpp AttackRoulette.cpp Polyomino.cpp Enemy.cpp AlphaEnemy.cpp Player.cpp Title.cpp Game.cpp Result.cpp Ranking.cpp Main.cpp \
+| sed 's/^#.*//' | sed '1s/^/#include <Siv3D.hpp>/'
