e506caf70af44b93a69cb7bc50209d63c7900e52
so-su <komiyama.masaki.53w@st.kyoto-u.ac.jp>
2023-03-19 09:37:33 +0900

    Playerのコードを整理

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,56 +1,57 @@
 #include "Player.hpp"
 
-Player::Player()
-    : hp(1000),
-      max_hp(1000),
-      ap(0),
-      max_ap(1000),
-      sp(0),
-      max_sp(1000),
-      hp_bar(Rect{100, 750, 800, 20}, MyColor::Green),
-      ap_bar(Rect{950, 750, 100, 20}, MyColor::Red),
-      sp_bar(Rect{1100, 750, 100, 20}, MyColor::Blue) {
+Player::Player() {
     hp_bar.update(static_cast<double>(hp) / max_hp);
     ap_bar.update(static_cast<double>(ap) / max_ap);
     sp_bar.update(static_cast<double>(sp) / max_sp);
 }
 
+// ダメージを受ける
 void Player::get_damaged(int32 value) {
     hp = Max(hp - value, 0);
     hp_bar.update(static_cast<double>(hp) / max_hp);
 }
 
+// HPを回復する
 void Player::get_healed(int32 value) {
     hp = Min(hp + value, max_hp);
     hp_bar.update(static_cast<double>(hp) / max_hp);
 }
 
+// APを得る
 void Player::get_ap(int32 value) {
     ap = Min(ap + value, max_ap);
     ap_bar.update(static_cast<double>(ap) / max_ap);
 }
 
+// SPを得る
 void Player::get_sp(int32 value) {
     sp = Min(sp + value, max_sp);
     sp_bar.update(static_cast<double>(sp) / max_sp);
 }
 
+// 生きているかを返す
 bool Player::is_alive() const { return hp > 0; }
 
+// APが満タンかを返す
 bool Player::ap_is_full() const { return ap == max_ap; }
 
+// SPが満タンかを返す
 bool Player::sp_is_full() const { return sp == max_sp; }
 
+// APを0にする
 void Player::reset_ap() {
     ap = 0.0;
     ap_bar.update(0.0);
 }
 
+// SPを0にする
 void Player::reset_sp() {
     sp = 0.0;
     sp_bar.update(0.0);
 }
 
+// HP,AP,SPのバーを描画する
 void Player::draw() const {
     hp_bar.draw();
     ap_bar.draw();
--- src/Player.hpp
+++ src/Player.hpp
@@ -6,42 +6,61 @@ class Player {
    public:
     Player();
 
+    // ダメージを受ける
     void get_damaged(int32 value);
 
+    // HPを回復する
     void get_healed(int32 value);
 
+    // APを得る
     void get_ap(int32 value);
 
+    // SPを得る
     void get_sp(int32 value);
 
+    // 生きているかを返す
     bool is_alive() const;
 
+    // APが満タンかを返す
     bool ap_is_full() const;
 
+    // SPが満タンかを返す
     bool sp_is_full() const;
 
+    // APを0にする
     void reset_ap();
-
+    
+    // SPを0にする
     void reset_sp();
 
+    // HP,AP,SPのバーを描画する
     void draw() const;
 
    private:
-    int32 hp;
+    // 現在のHP
+    int32 hp{max_hp};
 
-    int32 max_hp;
+    // 現在のAP
+    int32 ap{0};
 
-    int32 ap;
+    // 現在のSP
+    int32 sp{0};
+    
+    // 最大のHP
+    static constexpr int32 max_hp{1000};
+    
+    // 最大のAP
+    static constexpr int32 max_ap{1000};
+    
+    // 最大のSP
+    static constexpr int32 max_sp{1000};
 
-    int32 max_ap;
+    // HPのバー
+    ProgressBar hp_bar{Rect{100, 750, 800, 20}, MyColor::Green};
 
-    int32 sp;
+    // APのバー
+    ProgressBar ap_bar{Rect{950, 750, 100, 20}, MyColor::Red};
 
-    int32 max_sp;
-
-    ProgressBar hp_bar;
-
-    ProgressBar ap_bar;
-
-    ProgressBar sp_bar;
+    // SPのバー
+    ProgressBar sp_bar{Rect{1100, 750, 100, 20}, MyColor::Blue};
 };
